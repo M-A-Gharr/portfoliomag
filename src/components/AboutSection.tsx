4bd5778b75a1565143f66448e5f0d5e4bb5301b5
M-A-Gharr <exploredubaidiaries@gmail.com>
2025-04-26 19:22:34 +0100

    Auto Deploy 🚀

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -13,12 +13,12 @@ const AboutSection = () => {
         <div className="lg:w-1/3 flex flex-col items-center text-center">
           <div className="relative w-60 h-60 rounded-full overflow-hidden border-4 border-highlight mb-6 animate-fade-in">
             <img 
-              src="dist/images/profilecartoonblurdark.png"
+              src="/dist/images/profilecartoonblurdark.png"
               alt="Developer profile dark mode"
               className="w-full h-full object-cover dark:block hidden"
             />
             <img 
-              src="dist/images/profilecartoonblur.png" 
+              src="/dist/images/profilecartoonblur.png" 
               alt="Developer profile light mode"
               className="w-full h-full object-cover dark:hidden block"
             />
